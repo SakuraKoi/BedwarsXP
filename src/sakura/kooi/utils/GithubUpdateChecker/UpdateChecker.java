82ec759c28a45d0cadcbf2f979a5debb252e246e
SakuraKoi <sakurakoi993519867@gmail.com>
2020-04-21 15:52:34 +0800

    Close reader correctly (Release updated)

--- src/sakura/kooi/utils/GithubUpdateChecker/UpdateChecker.java
+++ src/sakura/kooi/utils/GithubUpdateChecker/UpdateChecker.java
@@ -1,6 +1,7 @@
 package sakura.kooi.utils.GithubUpdateChecker;
 
 import lombok.AllArgsConstructor;
+import lombok.Cleanup;
 import sakura.kooi.utils.GithubUpdateChecker.org.json.JSONObject;
 
 import java.io.BufferedReader;
@@ -21,13 +22,12 @@ public class UpdateChecker {
         URL url = new URL("https://api.github.com/repos/"+user+"/"+repo+"/releases/latest");
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+        @Cleanup BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         String line;
         StringBuilder result = new StringBuilder();
         while ((line = rd.readLine()) != null) {
             result.append(line);
         }
-        rd.close();
         JSONObject json = new JSONObject(result.toString());
         String tag = json.getString("tag_name");
         String link = json.getString("html_url");
