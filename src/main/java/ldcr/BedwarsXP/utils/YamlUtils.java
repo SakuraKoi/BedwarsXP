b0a767256cd50cb0e6d0a2141f2725af0b46f2da
Sakura Koi <sakurakoi993519867@gmail.com>
2021-11-07 00:18:02 +0800

    Refactor: 使用java api替代apache commons-io

--- src/main/java/ldcr/BedwarsXP/utils/YamlUtils.java
+++ src/main/java/ldcr/BedwarsXP/utils/YamlUtils.java
@@ -1,19 +1,16 @@
 package ldcr.BedwarsXP.utils;
 
-import lombok.Cleanup;
-import org.apache.commons.io.IOUtils;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.StringReader;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 
 public class YamlUtils {
 	public static YamlConfiguration loadYamlUTF8(File file) throws IOException {
-		@Cleanup FileInputStream fis = new FileInputStream(file);
-		String yaml = IOUtils.toString(fis, StandardCharsets.UTF_8);
+		String yaml = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
 		StringReader sr = new StringReader(yaml);
 		return YamlConfiguration.loadConfiguration(sr);
 	}
